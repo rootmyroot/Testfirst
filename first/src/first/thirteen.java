2dfd2f2933123a85d6d296329b903db7084d3bb1
USER <USER@ASUS>
2021-11-29 16:33:44 +0530

    commit this stuff

---
+++ first/src/first/thirteen.java
@@ -0,0 +1,16 @@
+package first;
+
+public class thirteen {
+	public static void main(String [] args) {
+		char gender = 'M';
+		switch(gender) {
+		case 'F':
+			System.out.println("female");
+			break;
+		case 'M':
+			System.out.println("male");
+			break;
+		}
+	}
+
+}
